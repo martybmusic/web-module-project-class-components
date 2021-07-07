9ec885054dd21a30744cbd1f1d1b1d963389d929
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-07-07 17:09:50 -0500

    Further Building

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const TodoForm = (props) => {
     return (
+        <div>
+            <h1>TodoList: MVP</h1>
+            <input type="text" placeholder="enter task" onChange={}/>
+            <button type="submit" onClick={}>
+                Add Todo
+            </button>
+            <button>
+                Clear Completed
+            </button>
+        </div>
 
     )
 }
